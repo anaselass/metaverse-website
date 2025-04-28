166364805eb9357e34ffaf51204fd32f832baa9d
anaselass <anas.elassri.01@gmail.com>
2025-04-28 16:06:44 +0100

    Add Banner and Banner2 components with responsive layouts and content

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import Banner from "./Components/Banner/Banner";
+import Banner2 from "./Components/Banner/Banner2";
 import Hero from "./Components/Hero/Hero";
 import Navbar from "./Components/Navbar/Navbar";
 import Quotes from "./Components/Quotes/Quotes";
@@ -10,7 +12,15 @@ function App() {
         <Hero />
         <Quotes />
         <div
-          className="w-[300px] h-[300px] bg-gradient-to-r from-primary to-secondary rounded-full 
+          className="w-[150px] h-[150px] sm:w-[250px] sm:h-[250px] md:w-[300px] md:h-[300px]  bg-gradient-to-r from-primary to-secondary rounded-full 
+          absolute top-0 left-0 blur-3xl animated-wrapper"
+        ></div>
+      </div>
+      <div className="relative">
+        <Banner />
+        <Banner2 />
+        <div
+          className="w-[150px] h-[150px] sm:w-[250px] sm:h-[250px] md:w-[300px] md:h-[300px]  bg-gradient-to-r from-primary to-secondary rounded-full 
           absolute top-0 left-0 blur-3xl animated-wrapper"
         ></div>
       </div>
---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,44 @@
+import BannerPng from "../../assets/banner1.png";
+import { BiPlayCircle } from "react-icons/bi";
+
+const Banner = () => {
+  return (
+    <div className="py-12 relative z-10 text-black dark:text-white">
+      <div className="container min-h-[520px] flex items-center">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 place-items-center">
+          {/* image section */}
+          <div>
+            <img
+              src={BannerPng}
+              alt="BannerPng"
+              className="w-full max-w-[400px]"
+            />
+          </div>
+          {/* text content section */}
+          <div className="space-y-5  order-2 sm:order-1 md:px-9 px-4">
+            <h1 className="text-4xl font-semibold">
+              GET READY TO ENJOY VR{" "}
+              <span className="bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary">
+                MOVIES WITH OUR PLATFORM
+              </span>
+            </h1>
+            <p className="">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+              quisquam ab, illo maiores magni dolorum repellat cumque atque
+              doloribus, et, id ipsum eaque architecto
+            </p>
+            <div className="flex gap-5">
+              <button className="primary-btn">Get Started</button>
+              <button className="flex gap-2 items-center cursor-pointer">
+                <BiPlayCircle className="text-3xl" />
+                See Demo
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
